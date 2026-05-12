37e38504b275df10a74505e602e1983395b1b75d
ItssBelieve <r2420058@students.uz.ac.zw>
2026-05-12 13:17:47 +0200

    Add script run entrypoint

--- finance/Calculations.py
+++ finance/Calculations.py
@@ -93,3 +93,8 @@ async def calculate_hire_purchase(request: InstallmentCalculationRequest):
             f"Monthly Payment: ${monthly_payment:,.2f}"
         )
     )
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8004)
